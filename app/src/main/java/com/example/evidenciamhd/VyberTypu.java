70bb6bc00ea3e6783f6e8b464b86321ebe632411
Jan_Melich <melich@spse-po.sk>
2019-04-02 11:08:26 +0200

    ahooooj

--- app/src/main/java/com/example/evidenciamhd/VyberTypu.java
+++ app/src/main/java/com/example/evidenciamhd/VyberTypu.java
@@ -5,21 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import static com.example.evidenciamhd.R.drawable.autobusy;
 
 public class VyberTypu extends AppCompatActivity {
 
-    private ImageButton buttonTrolejbusy;
-    private ImageButton buttonAutobusy;
-    private ImageButton buttonIne;
+    private ImageButton buttonTrolejbusy, buttonAutobusy, buttonIne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vyber_typu);
 
-        buttonTrolejbusy= findViewById(R.id.trolejbusy);
-        buttonAutobusy= findViewById(R.id.autobusy);
-        buttonIne= findViewById(R.id.technickeVozidla);
+        buttonTrolejbusy = findViewById(R.id.trolejbusy);
+        buttonAutobusy = findViewById(R.id.autobusy);
+        buttonIne = findViewById(R.id.technickeVozidla);
 
         buttonTrolejbusy.setOnClickListener(new View.OnClickListener() {
             @Override
