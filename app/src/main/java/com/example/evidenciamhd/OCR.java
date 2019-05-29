be344d67997172f07a95db2cced767b0869cafe9
Viktoria <ondrejovaviktoria@gmail.com>
2019-05-29 20:28:05 +0200

    ocr neukaze popup

--- app/src/main/java/com/example/evidenciamhd/OCR.java
+++ app/src/main/java/com/example/evidenciamhd/OCR.java
@@ -1,7 +1,11 @@
 package com.example.evidenciamhd;
 
 import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +15,8 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
@@ -19,6 +25,7 @@ import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class OCR extends AppCompatActivity {
 
@@ -26,14 +33,18 @@ public class OCR extends AppCompatActivity {
     TextView textView;
     CameraSource cameraSource;
     final int RequestCameraPermissionID = 1001;
+    String result = "nic";
+    SparseArray<TextBlock> items;
+    Dialog dialog;
+    TextView txtResult;
+    Button ano, nie;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case RequestCameraPermissionID:
-            {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+        switch (requestCode) {
+            case RequestCameraPermissionID: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                         return;
                     }
                     try {
@@ -50,6 +61,7 @@ public class OCR extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ocr);
+        dialog = new Dialog(this);
 
         camera = findViewById(R.id.surfaceView);
         textView = findViewById(R.id.text_view);
@@ -70,14 +82,13 @@ public class OCR extends AppCompatActivity {
 
                     try {
                         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-
                             ActivityCompat.requestPermissions(OCR.this,
-                                    new String []{Manifest.permission.CAMERA},
+                                    new String[]{Manifest.permission.CAMERA},
                                     RequestCameraPermissionID);
                             return;
                         }
                         cameraSource.start(camera.getHolder());
-                    }catch (IOException e){
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
 
@@ -85,8 +96,6 @@ public class OCR extends AppCompatActivity {
 
                 @Override
                 public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                    Log.d("mhd", "surface changed");
-
                 }
 
                 @Override
@@ -96,36 +105,83 @@ public class OCR extends AppCompatActivity {
                 }
             });
 
-            textRecognizer.setProcessor(new Detector.Processor<TextBlock>(){
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
 
                 @Override
-                public void release() {
-
-                }
+                public void release() {}
 
                 @Override
                 public void receiveDetections(Detector.Detections<TextBlock> detections) {
-                    Log.d("camera", "som tu");
+                    items = detections.getDetectedItems();
+                    if (items.size() != 0) {
 
-                    final SparseArray<TextBlock> items = detections.getDetectedItems();
-                    Log.d("camera", " velkost " + items.size());
-                    if (items.size() != 0){
-                        textView.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                StringBuilder stringBuilder = new StringBuilder();
-                                for (int i = 0 ; i < items.size(); ++i){
-                                    TextBlock item = items.valueAt(i);
-                                    stringBuilder.append(item.getValue());
-                                    stringBuilder.append("\n");
-                                }
-                                textView.setText(stringBuilder.toString());
-                            }
-                        });
+                        StringBuilder stringBuilder = new StringBuilder();
+                        for (int i = 0; i < items.size(); ++i) {
+                            TextBlock item = items.valueAt(i);
+                            stringBuilder.append(item.getValue());
+                            stringBuilder.append("\n");
+                        }
+                        textView.setText(stringBuilder);
+                        String vysledok = verification();
+                        Log.d("vysledok", "vysledok: " + vysledok);
+                        if(!vysledok.equals("failed")){
+                            showPopUp();
+                        }
+
                     }
                 }
             });
         }
 
     }
+
+    public void showPopUp(){
+
+        txtResult = dialog.findViewById(R.id.txtResultDrawable);
+        ano = dialog.findViewById(R.id.anoButton);
+        nie = dialog.findViewById(R.id.nieButton);
+
+        dialog.setContentView(R.layout.pop_up);
+        txtResult.setText(result);
+
+        ano.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int mapka = Integer.parseInt(result);
+                Intent intent = new Intent(OCR.this, InformacieOVozidle.class);
+                intent.putExtra("mapka",mapka);
+                startActivity(intent);
+            }
+        });
+
+        nie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+    public String verification() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < items.size(); ++i) {
+            TextBlock item = items.valueAt(i);
+            stringBuilder.append(item.getValue());
+            Log.d("item", "item: "+ stringBuilder);
+            if (Pattern.matches("[0-9]{3}", stringBuilder.toString())) {
+                result = stringBuilder.toString();
+                Log.d("result", "vysledok po podmienke : " + result);
+                return result;
+            }
+            stringBuilder.setLength(0);
+        }
+        return "failed";
+    }
+
+
+
 }
